9e8b66d83c89c02279bad45e915f9d55a3fe7d23
tufan@kaynak.gen.tr <!r0nM4!d3n!>
2019-10-28 23:19:18 +0300

    Project initiation

--- trtcsbuts_integration/config/docs.py
+++ trtcsbuts_integration/config/docs.py
@@ -3,8 +3,8 @@ Configuration for docs
 """
 
 
-# source_link = "https://github.com/[org_name]/trtcsbuts_integration"
-# docs_base_url = "https://[org_name].github.io/trtcsbuts_integration"
+# source_link = "https://github.com/Framras/trtcsbuts_integration"
+# docs_base_url = "https://Framras.github.io/trtcsbuts_integration"
 # headline = "App that does everything"
 # sub_heading = "Yes, you got that right the first time, everything"
 
