3714cc483b4ebc56ea07f92903b723ede37c15a4
tufan@kaynak.gen.tr <!r0nM4!d3n!>
2019-10-29 22:02:12 +0300

    connection in api refined

--- trtcsbuts_integration/api.py
+++ trtcsbuts_integration/api.py
@@ -4,38 +4,40 @@ import json
 
 
 def communicate_with_uts(servicepath, servicerequestdatafields):
-    # Select the server according to the mode of the integration
-    if frappe.db.get_single_value("TR TCSB UTS Integration Settings", "test") == 1:
-        url = frappe.db.get_single_value("TR TCSB UTS Integration Settings", "testserver")
-        utstoken = frappe.db.get_single_value("TR TCSB UTS Integration Settings", "testsystemtoken")
-    else:
-        url = frappe.db.get_single_value("TR TCSB UTS Integration Settings", "realserver")
-        utstoken = frappe.db.get_single_value("TR TCSB UTS Integration Settings", "systemtoken")
+    # This should only be used if the integration is enabled
+    if frappe.db.get_single_value("TR TCSB UTS Integration Settings", "enable") == 1:
+        # Select the server according to the mode of the integration
+        if frappe.db.get_single_value("TR TCSB UTS Integration Settings", "test") == 1:
+            url = frappe.db.get_single_value("TR TCSB UTS Integration Settings", "testserver")
+            utstoken = frappe.db.get_single_value("TR TCSB UTS Integration Settings", "testsystemtoken")
+        else:
+            url = frappe.db.get_single_value("TR TCSB UTS Integration Settings", "realserver")
+            utstoken = frappe.db.get_single_value("TR TCSB UTS Integration Settings", "systemtoken")
 
-    # her web servis çağrısının başlık (header) kısmına utsToken etiketiyle sistem token’ının değerini eklemelidir
-    __headers = {
-        'utsToken': utstoken,
-        'Content-Type': frappe.db.get_single_value("TR TCSB UTS Integration Settings", "contenttype")
-    }
-    _requesturl = url + servicepath
+        # her web servis çağrısının başlık (header) kısmına utsToken etiketiyle sistem token’ının değerini eklemelidir
+        __headers = {
+            'utsToken': utstoken,
+            'Content-Type': frappe.db.get_single_value("TR TCSB UTS Integration Settings", "contenttype")
+        }
+        _requesturl = url + servicepath
 
-    s = requests.Session()
-    s.headers.update(__headers)
-    # Web servislerin tamamında HTTP request method olarak “POST” metodu kullanılmaktadır.
-    response = s.post(_requesturl, servicerequestdatafields)
+        s = requests.Session()
+        s.headers.update(__headers)
+        # Web servislerin tamamında HTTP request method olarak “POST” metodu kullanılmaktadır.
+        response = s.post(_requesturl, servicerequestdatafields)
 
-    # For successful API call, response code will be 200 (OK)
-    if response.ok:
-        return json.loads(response.content)
+        # For successful API call, response code will be 200 (OK)
+        if response.ok:
+            return json.loads(response.content)
 
-    # # Loading the response data into a dict variable # json.loads takes in only binary or string variables so
-    # using content to fetch binary content # Loads (Load String) takes a Json file and converts into python data
-    # structure (dict or list, depending on JSON) jData = json.loads(response.content) print("The response
-    # contains {0} properties".format(len(jData))) print("\n") for key in jData: #        print(key + " : " +
-    # jData[key]) for subkey in jData[key]: print(subkey + " : " + jData[key][subkey])
-    else:
-        # If response code is not ok (200), print the resulting http error code with description
-        response.raise_for_status()
+        # # Loading the response data into a dict variable # json.loads takes in only binary or string variables so
+        # using content to fetch binary content # Loads (Load String) takes a Json file and converts into python data
+        # structure (dict or list, depending on JSON) jData = json.loads(response.content) print("The response
+        # contains {0} properties".format(len(jData))) print("\n") for key in jData: #        print(key + " : " +
+        # jData[key]) for subkey in jData[key]: print(subkey + " : " + jData[key][subkey])
+        else:
+            # If response code is not ok (200), print the resulting http error code with description
+            response.raise_for_status()
 
 
 # FİRMA SORGULAMA SERVİSİ
